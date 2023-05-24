e4ecb8354283969af57ea89b4f836b2d74c541e1
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-24 23:54:30 +0530

    react practice

--- src/ParentProps.js
+++ src/ParentProps.js
@@ -9,3 +9,17 @@
 
 // export default ParentProps
 
+import React, { useState } from 'react'
+import ChildProps from './ChildProps'
+function ParentProps() {
+  const [color,setColor]=useState("default");
+   const ChangeColor =(color)=> {
+    setColor(color)
+   }
+  return (
+    <>{"this is parent component color " + color}
+    <ChildProps ChangeColor={ChangeColor} /></>
+  )
+}
+
+export default ParentProps
